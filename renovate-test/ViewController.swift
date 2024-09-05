dc9f81a5980c594c6ef9c0ee58669a897d61d887
Daisuke Okazaki <dk@qrnk.jp>
2024-09-05 14:09:52 +0900

    初期コミット

---
+++ renovate-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  renovate-test
+//
+//  Created by 岡崎 大典 on 2024/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
