c8eab50147ecdbe163bdf5133d7ad1428fb989eb
Daisuke Okazaki <dk@qrnk.jp>
2024-09-06 11:31:09 +0900

    SPMのテスト

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '12.0'
 
 target 'renovate-test' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -8,6 +8,6 @@ target 'renovate-test' do
   # Pods for renovate-test
   
   # Braze
-  pod 'BrazeKit', '5.14.0'
+#  pod 'BrazeKit', '5.14.0'
 
 end
--- spm-test/Package.swift
+++ spm-test/Package.swift
@@ -5,19 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "spm-test",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "spm-test",
             targets: ["spm-test"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.5.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "spm-test"),
+            name: "spm-test",
+            dependencies: [
+                "Alamofire"
+            ]
+        ),
         .testTarget(
             name: "spm-testTests",
-            dependencies: ["spm-test"]),
+            dependencies: ["spm-test"]
+        ),
     ]
 )
